24aabb40f6c45d8e6511015748a949a0203bc466
Takefumi Ota <8305@dragonparts.com>
2016-02-09 18:07:27 +0900

    Add HTML DOM check. (Can't move)

--- twig/SnippetTwigExtension.php
+++ twig/SnippetTwigExtension.php
@@ -56,7 +56,24 @@ class SnippetTwigExtension extends \Twig_Extension
         // Return null if file not exists.
         $filename = realpath(USER_DIR . $dir . '/' . $content);
         if ($filename) {
-            return file_get_contents($filename);
+
+            // Check HTML ?
+            if ($this->config['plugins.snippet.check_html_dom']) {
+                libxml_use_internal_errors(true); // Switch error check to user mode
+                $html = file_get_contents($filename);
+                $doc = new \DOMDocument();
+                if (!$doc->loadHTML($html)) {
+                    var_dump(libxml_get_errors());
+
+                } else {
+                    return $html;
+                }
+            } else {
+                return file_get_contents($filename);
+
+            }
+
+
         } else {
             return '';
         }
